258da19acbb29a191a223e6a7cf8575f0201353b
sameowrai <sameeowrai@gmail.com>
2024-10-27 08:15:12 +0330

    Edit main.cpp to define '\n' char in encoding and decoding

--- main.cpp
+++ main.cpp
@@ -6,8 +6,13 @@ using namespace std;
 string Encode(string text){
     string result = "";
     for(int i=0; i < text.length(); i++){
-        int AsciiValue = int(text[i]);
-        string newValue = to_string(AsciiValue*10);
+        string newValue;
+        if(text[i] == '\n'){
+            newValue = "9999";
+        }else{
+            int AsciiValue = int(text[i]);
+            newValue = to_string(AsciiValue*10);
+        }
         while(newValue.length()<4){
             newValue = "0" + newValue;
         }
@@ -20,7 +25,11 @@ string Decode(string text) {
     string result = "";
     for (size_t i = 0; i < text.length(); i += 4) {
         int value = stoi(text.substr(i, 4)) / 10;
-        result += static_cast<char>(value); 
+        if(value==9999){
+            result += "\n";
+        }else{
+            result += static_cast<char>(value); 
+        }
     }
     return result;
 }
