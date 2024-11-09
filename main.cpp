1d6b981dc5c9b5f3fd4ce70f1b881b03d18944af
Sameeowrai <sameeowrai@gmail.com>
2024-11-10 01:46:37 +0330

    Create tenenc hpp file

--- main.cpp
+++ main.cpp
@@ -1,47 +1,4 @@
-#include <iostream>
-#include <fstream>
-
-using namespace std;
-
-string Encode(string text){
-    string result = "";
-    for(int i=0; i < text.length(); i++){
-        string newValue;
-        if(text[i] == '\n'){
-            newValue = "9999";
-        }else{
-            int AsciiValue = int(text[i]);
-            newValue = to_string(AsciiValue*10);
-        }
-        while(newValue.length()<4){
-            newValue = "0" + newValue;
-        }
-        result += newValue;
-    }
-    return result;
-}
-
-string Decode(string text) {
-    string result = "";
-    for (size_t i = 0; i < text.length(); i += 4) {
-        int value = stoi(text.substr(i, 4)) / 10;
-        if(value==9999){
-            result += "\n";
-        }else{
-            result += static_cast<char>(value); 
-        }
-    }
-    return result;
-}
-
-int Find(string str, char* argv[], int size){
-    for(int i=0; i < size; i++){
-        if(string(argv[i]) == str){
-            return i;
-        }
-    }
-    return -1;
-}
+#include "tenenc.hpp"
 
 int main(int argc, char* argv[]) {
     int op;
